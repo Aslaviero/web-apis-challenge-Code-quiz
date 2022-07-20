cc794efeeca9d3c4d79e0111dbf434e28117db70
Anthony Slaviero <tonyslaviero@gmail.com>
2022-07-19 23:58:20 -0700

    final commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # web-apis-challenge-Code-quiz
-web apis challenge: Code quiz
+- web apis challenge: Code quiz
+- Wrote base HTML ID Tags/buttons/form/input for Js
+- Wrote CSS styling for buttons to add color and alignment
+- 
---
+++ index.js
@@ -0,0 +1,155 @@
+// 
+var questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
+    ],
+    answer: "all of the above"
+  },
+  {
+    title:
+      "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes"
+  },
+  {
+    title:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+    answer: "console.log"
+  }
+];
+
+var highscores = []
+var i = 0;
+var time = 60;
+var score = 0;
+// shows questions when start button is clicked
+document.querySelector("#start").onclick = () => {
+
+  showQuestions();
+ 
+// Set countdown timer and create h3 tag for display
+  setInterval(() => {
+    if (time > 0 && i < questions.length)
+      time--
+    document.querySelector("#time").innerHTML =
+      `<h3>${time}</h3>`
+
+  }, 900)
+// Set up H3 to show score of player
+  {
+    document.querySelector("#score").innerHTML =
+      `<h3>${score}</h3>`
+
+
+  }
+}
+
+// function of if loop to show answer buttons and questions
+function showQuestions() {
+
+  if (i < questions.length) {
+    document.querySelector("#questions").innerHTML = `
+    <h3>${questions[i].title}</h3>
+    <button class="answers">${questions[i].choices[0]}</button>
+    <button class="answers">${questions[i].choices[1]}</button>
+    <button class="answers">${questions[i].choices[2]}</button>
+    <button class="answers">${questions[i].choices[3]}</button>
+    `
+    // Hide Start button after game starting game
+    document.querySelector('#start').setAttribute("style", "visibility:hidden")
+  }
+
+}
+
+function endGame() {
+  document.querySelector('#check').innerHTML =
+    `<h3>Game Over</h3>`
+  document.querySelector('#questions').innerHTML = ""
+  document.querySelector('#highscores').setAttribute("style", "visibility:visible")
+  document.querySelector('#time').setAttribute("style", "visibility:hidden")
+}
+//created submit button for name at end of game
+document.querySelector("#names").addEventListener("submit", function (event) {
+  event.preventDefault()
+  var username = document.querySelector("#namefield").value
+  var currentscores = JSON.parse(localStorage.getItem("scores"))
+  if (currentscores) {
+    highscores = currentscores
+  }
+  highscores.push({
+    name: username,
+    score: score,
+  })
+  // Local storage to store scores and names 
+  localStorage.setItem("scores", JSON.stringify(highscores))
+  var allscores = document.querySelector("#allscores")
+  for (var x = 0; x < highscores.length; x++) {
+    var userscore = document.createElement("div")
+    userscore.innerText = highscores[x].name
+    allscores.appendChild(userscore)
+  }
+  
+ 
+})
+//To show if answer is correct or wrong
+document.querySelector("#questions").onclick = function (event) {
+  event.preventDefault()
+  console.log(event.target.innerText)
+  console.log(i)
+  if (event.target.innerText === questions[i].answer) {
+    document.querySelector('#check').innerHTML =
+      `<h3>Correct</h3>`
+    {
+      score += 10;
+    }
+  } else if (event.target.innerText != questions[i].answer) {
+    document.querySelector('#check').innerHTML =
+      `<h3>Wrong</h3>`
+    time -= 10;
+    score -= 10;
+  }
+
+  document.querySelector("#score").innerHTML =
+    `<h3>${score}</h3>`
+
+  i++
+  if (i < questions.length) {
+    showQuestions()
+  }
+  else {
+    endGame()
+    
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
